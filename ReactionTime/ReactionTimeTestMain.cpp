e46497eb0cbb32392ffb114ba6f7ac26d2c639ce
isaacdd <isaacdd.bcn.2001@gmail.com>
2021-03-17 00:55:18 +0100

    finish

--- ReactionTime/ReactionTimeTestMain.cpp
+++ ReactionTime/ReactionTimeTestMain.cpp
@@ -4,39 +4,41 @@
 
 int main(int argc, char** argv)
 {
-	bool active(true);
 	SDL_Init(SDL_INIT_VIDEO);
-	SDL_Window* window = SDL_CreateWindow("", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, 0);
+	SDL_Window* window = SDL_CreateWindow("", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 400, 300, 0);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 	srand(time(0));
-	int state(0);
-	int states[5]{ 1,4,3,1,1 };
-	Uint32 time = SDL_GetTicks();
-	Uint32 time2wait = rand() % 5000;
-	Uint32 time2click = 0;
-
-	int color[5][4] =
+	Uint32 state(0), time(0), last(0), best(1000);
+	int color[3][4]{ 0,0,0,255,255,0,0,255,0,255,0,255 };
+	while (1)
 	{
-		0,0,0,255,
-		255,0,0,255,
-		0,255,0,255,
-		0,0,255,255,
-		255,255,0,255
-	};
-
-	while (active)
-	{
+		if (state == 1 && SDL_GetTicks() > time) state = 2;
 		SDL_Event event;
 		while (SDL_PollEvent(&event))
 		{
-			if (event.type == SDL_QUIT) active = false;
-			if (event.button.button == SDL_BUTTON_LEFT && event.type == SDL_MOUSEBUTTONDOWN) state = states[state];
+			if (event.type == SDL_QUIT) return 0;
+			if (event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT)
+			{
+				switch (state)
+				{
+				case 0:
+					time = SDL_GetTicks() + rand() % 5000;
+					last = 0;
+					state = 1;
+					break;
+				case 1:
+					state = 0;
+					break;
+				case 2:
+					last = SDL_GetTicks() - time;
+					if (last < best) best = last;
+					state = 0;
+					break;
+				}
+			}
 		}
-		if (state == 1 && SDL_GetTicks() > time + time2wait) state = 2;
-		if (state == 2) time2click = SDL_GetTicks() - time2wait;
-
 		static char title[256];
-		sprintf_s(title, 256, " | STATE %d | TIME2WAIT %d | TIME2CLICK %d | ", state, time2wait, time2click);
+		sprintf_s(title, 256, " | best %d | last %d | ", best, last);
 		SDL_SetWindowTitle(window, title);
 		SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
 		SDL_SetRenderDrawColor(renderer, color[state][0], color[state][1], color[state][2], color[state][3]);
